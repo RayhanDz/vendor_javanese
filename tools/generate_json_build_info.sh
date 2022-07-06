6e1f1063fe7da253dda5351a061f248376834d40
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:57:15 +0700

    Updated

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -9,8 +9,8 @@ then
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $out/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    maintainer=$(grep org\.komodo\.maintainer $out/system/build.prop | cut -d= -f2)
-    version=$(grep -m1 org\.komodo\.version $out/system/build.prop | cut -d= -f2);
+    maintainer=$(grep org\.javanese\.maintainer $out/system/build.prop | cut -d= -f2)
+    version=$(grep -m1 org\.javanese\.version $out/system/build.prop | cut -d= -f2);
     echo "{" > $file_path.json
     echo "  \"error\"       : false," >> $file_path.json
     echo "  \"id\"          : \"$id\"," >> $file_path.json
@@ -20,12 +20,10 @@ then
     echo "  \"size\"        : $file_size," >> $file_path.json
     echo "  \"filehash\"    : \"$md5\"," >> $file_path.json
     echo "  \"url\"         : \"\"," >> $file_path.json
-    echo "  \"website_url\" : \"https://komodo-os.my.id\"," >> $file_path.json
-    echo "  \"news_url\"    : \"https://t.me/KomodOSRom\"," >> $file_path.json
-    echo "  \"donate_url\"  : \"https://www.paypal.me/KomodoOS\"," >> $file_path.json
+    echo "  \"news_url\"    : \"https://t.me/JavaneseOSupdates\"," >> $file_path.json
     echo "  \"maintainer\"  : \"$maintainer\"," >> $file_path.json
     echo "  \"maintainer_url\" : \"https://github.com/\"," >> $file_path.json
-    echo "  \"forum_url\"      : \"https://t.me/KomodOSGroup\"" >> $file_path.json
+    echo "  \"forum_url\"      : \"https://t.me/JavaneseCommunityOfficial\"" >> $file_path.json
     echo "}" >> $file_path.json
   fi
 fi
