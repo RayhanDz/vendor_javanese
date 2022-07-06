21ef4417f92abbfa05aa0944944baf6d7a7f9e03
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:36:04 +0700

    Updates

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 lunch_others_targets=()
-for device in $(python vendor/komodo/tools/get_official_devices.py)
+for device in $(python vendor/javanese/tools/get_official_devices.py)
 do
     for var in user userdebug eng; do
-        lunch_others_targets+=("komodo_$device-$var")
+        lunch_others_targets+=("javanese_$device-$var")
     done
 done
