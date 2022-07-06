a02231b03dfe059005e548caf5c3cbbe597e6cf3
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:15:11 +0700

    Add

---
+++ build/tools/banner.sh
@@ -0,0 +1,17 @@
+# Specify colors utilized in the terminal
+red='\033[0;31m'             #  red
+grn='\033[0;32m'             #  green
+ylw='\033[0;33m'             #  yellow
+blu='\033[0;34m'             #  blue
+ppl='\033[0;35m'             #  purple
+cya='\033[0;36m'             #  cyan
+
+       echo -e ${grn}"×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=××=×=×=×=×=×=×=×=×=×"
+       echo -e ${grn}"*   aMMb                                                                     aMMMb   dMMMb   "
+       echo -e ${ylw}"*       dMP           AmmP        Vmp            Vmp         Ammp           dMP dMP dMP  VP  "
+       echo -e ${ylw}"*       dMP        AmmP   DMmp      vmp        Vmp       Ammp    Ammp       dMP dMP  VMMMb   "
+       echo -e ${ylw}"*       dMP     AMmP  Vmmp  DMmp      vmp   vmp        Ammp  VmmP  Ammp     dMP aMP dP  dMP  "
+       echo -e ${ppl}"* Dmmp VmmP   AmmP            Dmmp      vmp          AmmP            Amp     VMMMP   VMMMP   "
+       echo -e ${cya}"* THANKS FOR CHOOSING MAGNUS OS, HAPPY FLASHING!                          "
+       echo -e ${grn}"×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×==×=×=××=×=×=×=×=×=×=×=×=×"
+     
