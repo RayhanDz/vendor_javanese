d380f7fe353a77e34b29e04930517af0e250801d
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:17:17 +0700

    Vendor: java

--- build/templates/extract-files.sh
+++ build/templates/extract-files.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
 # Copyright (C) 2018 The PixelExperience Project
-# Copyright (C) 2019-2020 The Komodo OS Project
+# Copyright (C) 2019-2020 The Javanese OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 KOMODO_ROOT="${MY_DIR}/../../.."
 
-HELPER="${KOMODO_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${JAVANESE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -62,7 +62,7 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${KOMODO_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${JAVANESE_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
