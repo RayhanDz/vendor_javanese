5cb321c6d17631740d3b25fc79e5aabc5146da85
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:30:24 +0700

    Updated

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func komodoExpandVariables(ctx android.ModuleContext, in string) string {
-	komodoVars := ctx.Config().VendorConfig("komodoVarsPlugin")
+func javaneseExpandVariables(ctx android.ModuleContext, in string) string {
+	javaneseVars := ctx.Config().VendorConfig("javaneseVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if komodoVars.IsSet(name) {
-			return komodoVars.String(name), nil
+		if javaneseVars.IsSet(name) {
+			return javaneseVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
