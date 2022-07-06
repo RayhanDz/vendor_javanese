2e16b34290c438575cd5a97206900b7303c875aa
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-07-06 11:33:57 +0700

    Rebase

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -3,7 +3,7 @@ $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
 
 # Include board/platform macros
-include vendor/komodo/build/core/utils.mk
+include vendor/javanese/build/core/utils.mk
 
 # Include vendor platform definitions
-include vendor/komodo/build/core/vendor/*.mk
+include vendor/javanese/build/core/vendor/*.mk
